8b9c4ce93ee5c448823139cdfd31b83bd8e8669b
ecalot <ecalot>
2003-10-23 14:21:05 +0000

    o Moved Mid compiling function to mid o Changed some prototypes

--- PR/src/include/mid.h
+++ PR/src/include/mid.h
@@ -1,5 +1,11 @@
 
 #ifndef _MID_H_
 #define _MID_H_
+
+#include <stdio.h>
+#include "resources.h"
+
+char mFormatCompileMid(unsigned char* data, FILE* fp, tResource *res);
 char mFormatExtractMid(unsigned char* data, char *vFileext,unsigned long int size);
+
 #endif
