a4269833236df6414ca2ade232cf4f1d47235f7d
ecalot <ecalot>
2003-12-03 16:03:52 +0000

    o Added two function headers

--- PR/src/include/disk.h
+++ PR/src/include/disk.h
@@ -2,12 +2,17 @@
 #ifndef _DISK_H_
 #define _DISK_H_
 
+#include <stdio.h>
+
+/* 64 Kb */
 #define SIZE_OF_FILE    64*1024
 
-void repairFolders(char* a);
-char writeData(const unsigned char* data, int ignoreChars, char* vFileext, int size);
-int makebase(const char* p);
-int mLoadFileArray(const char* vFile,unsigned char** array);
-char mSaveRaw(const char* vFile,const unsigned char* output, int size);
+char mDiskVealidateFileHeader(unsigned char* text, int size, FILE* fp);
+char mSaveRaw      (const char* vFile,const unsigned char* output, int size);
+char writeData     (const unsigned char* data, int ignoreChars, char* vFileext, int size);
+char writeOpen     (char* vFileext, FILE* *fp);
+int  mLoadFileArray(const char* vFile,unsigned char** array);
+int  makebase      (const char* p);
+void repairFolders (char* a);
 
 #endif
