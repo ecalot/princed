3eeba2a6719e12ed2cbac518b307a23ba049ede2
ecalot <ecalot>
2004-06-16 17:01:07 +0000

    added new interfaces for hof and sav files handling

---
+++ FP/src/include/hof.h
@@ -0,0 +1,4 @@
+void hofAddRecord(char* name, float time); /* time is in seconds. The interface may be changed */
+void hofRead(char** names, float** times); /* idem */
+
+/* More functions may be added */
---
+++ FP/src/include/sav.h
@@ -0,0 +1,3 @@
+
+void savSave(int level, int lives, float time); /* Interface may be changed */
+void savLoad(int* level, int* lives, float* time); /* Interface may be changed */
