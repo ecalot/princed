0b01946db2640f8241b7df82e06b536642b852c7
ecalot <ecalot>
2004-06-22 21:31:41 +0000

    headers

--- FP/src/include/maps.h
+++ FP/src/include/maps.h
@@ -1,5 +1,38 @@
+/*  FreePrince - POP1 remake
+    Copyright (C) 2003, 2004 Princed Development Team
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    The authors of this program may be contacted at http://forum.princed.com.ar
+*/
+
+/*
+maps.c: Freeprince : Map handling library
+¯¯¯¯¯¯
+ Copyright 2003,2004 Princed Development Team
+  Created: 20 Jun 2004
+
+  Author: Enrique Calot <ecalot.cod@princed.com.ar>
+
+ Note:
+  DO NOT remove this copyright notice
+*/
 
 #include "map_defs.h"
+
+typedef enum {eLeft=0,eRight=1,eUp=2,eDown=3}tDirection;
 
 int levelUse(void* level);
 /* Sets the level for future use using the resource given
