1b86a456d301d4abc4b7ff0fd7da3a455480f4c4
unlord <unlord>
2004-07-08 13:13:28 +0000

    Added FPPalette object

---
+++ stuff/ideas/FPPalette.h
@@ -0,0 +1,13 @@
+#ifndef _FP_PALETTE_H
+#define _FP_PALETTE_H
+
+#include "FPResource.h"
+
+typedef struct FPPalette FPPalette;
+
+struct FPPalette {
+  FPResourceType type;
+  int id;
+};
+
+#endif
