c8e617fa876f66cb401fcca60b9f484122de231f
ecalot <ecalot>
2004-07-19 13:28:19 +0000

    added the kid object

---
+++ FP/src/include/object.h
@@ -0,0 +1,51 @@
+/*  Princed V3 - Prince of Persia Level Editor for PC Version
+    Copyright (C) 2003 Princed Development Team
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    The authors of this program may be contacted at http://forum.princed.com.ar
+*/
+
+/*
+kid.h: Free Prince : Kid object
+¯¯¯¯¯
+ Copyright 2004 Princed Development Team
+  Created: 19 Jul 2004
+
+  Author: Enrique Calot <ecalot.cod@princed.com.ar>
+
+ Note:
+  DO NOT remove this copyright notice
+*/
+
+#ifndef _KID_H_
+#define _KID_H_
+
+#include "resources.h"
+#include "input.h"
+
+typedef struct {
+	int location;
+	int floor;
+	int direction;
+	int framesLeft;
+	tData* action;
+} tKid;
+
+void kidInit(tKid* kid);
+void kidDraw(tKid* kid);
+void kidMove(tKid* kid,tKey* key);
+
+#endif
