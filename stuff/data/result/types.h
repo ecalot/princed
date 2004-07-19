7d908c5579ae0bca8f61ba95e6162e8abc07576a
ecalot <ecalot>
2004-07-19 20:11:24 +0000

    added parcially support for image offsets

--- stuff/data/result/types.h
+++ stuff/data/result/types.h
@@ -1,10 +1,7 @@
 /* Defined types for freeprince resources*/
 
-#define RES_TYPE_IMG_TR_LEFT 0
-#define RES_TYPE_IMG_TR_RIGHT 1
-#define RES_TYPE_IMG_BL_LEFT 2
-#define RES_TYPE_IMG_BL_RIGHT 3
+#define RES_TYPE_IMG 0
+#define RES_TYPE_IMG_PALETTED 1
 #define RES_TYPE_SND_MIDI 4
 #define RES_TYPE_SND_WAVE 5
-#define RES_TYPE_IMG_PALETTED 7
 #define RES_TYPE_LVL 6
