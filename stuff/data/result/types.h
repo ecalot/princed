8072cc84bd16e84e837b82d1fa427dca0300ad83
ecalot <ecalot>
2004-08-24 04:42:20 +0000

    deprecated

--- stuff/data/result/types.h
+++
@@ -1,7 +0,0 @@
-/* Defined types for freeprince resources*/
-
-#define RES_TYPE_IMG 0
-#define RES_TYPE_IMG_PALETTED 1
-#define RES_TYPE_SND_MIDI 4
-#define RES_TYPE_SND_WAVE 5
-#define RES_TYPE_LVL 6
