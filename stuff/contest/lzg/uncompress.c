7e6aa9f6471ce8f0cf77623b720ccf2905d0a9a3
ecalot <ecalot>
2005-03-09 21:17:52 +0000

    included the h calculations in the array

--- stuff/contest/lzg/uncompress.c
+++ stuff/contest/lzg/uncompress.c
@@ -64,15 +64,11 @@ void expandLzg(const unsigned char* array, int arraySize,
 				while (rep--) {
 					location=location%MAX_MXD_SIZE_IN_LZG; /* location is in range 0-1023 */
 
-					h=(cursor-location)/MAX_MXD_SIZE_IN_LZG;
 					/*
 					 * if the image is stored in an array of 1024 x n bytes
 					 * "h" is the height and "location" is the width
 					 */
-					img[cursor]=img[
-						h*MAX_MXD_SIZE_IN_LZG+
-						location
-					];
+					img[cursor]=img[(cursor-location)-((cursor-location)%MAX_MXD_SIZE_IN_LZG)+location];
 
 					cursor++;
 					location++;
